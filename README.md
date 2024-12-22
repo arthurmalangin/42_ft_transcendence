6e031fbcc855d2c2c24158be0b8eed7082cff3e0
arthurmalangin <amalangin@icloud.com>
2024-12-22 06:40:47 +0100

    start login

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # 42_ft_transcendence
 Surprise.
+
+# Setup Django
+Installation python3 et pip :
+```shell
+sudo apt install python3 python3-pip
+```
+
+Créer un environnement virtuel :
+```shell
+python3 -m venv envpython
+```
+
+Activer l’environnement virtuel :
+```shell
+source envpython/bin/activate
+```
+
+Installer Django dans l’environnement virtuel :
+```shell
+pip install django
+```
+
+Désactiver l’environnement virtuel :
+```shell
+deactivate
+```
+
+Créer projet Django :
+```shell
+django-admin startproject projectname
+```
+
+Lancer le serveur http://127.0.0.1:8000
+```shell
+cd projectname
+python manage.py runserver
+```
+
+django-admin startapp user
+
+docker compose up --build -d
+
+Postgre
+```shell
+pip install psycopg2-binary
+```
---
+++ main/login/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view, parser_classes
+from .models import Credential
+
+def index(request):
+	return HttpResponse("Hello, world. You're at the polls index.")
+
+@api_view(['POST'])
+def register(request):
+	username = request.POST.get("username")
+	email = request.POST.get("email") #todo
+	password = request.POST.get("password")
+	if username and password:
+		if Credential.objects.filter(user_name=username).exists():
+			return HttpResponse("user deja pris.", status=400)
+		crednetial = Credential(user_name=username, password=password)
+		crednetial.save()
+		return HttpResponse("user registered")
+	else:
+		return HttpResponse("Error: missing password or username.", status=400)
+	#return HttpResponse("Hello, world. You're at the polls index.")
+
+@api_view(['POST'])
+def login(request):
+	username = request.POST.get("username")
+	email = request.POST.get("email")
+	password = request.POST.get("password")
+	if username and password:
+		try:
+			credential = Credential.objects.get(user_name=username, password=password)
+			return HttpResponse("Login Ok ! Welcome")
+		except Credential.DoesNotExist:
+			return HttpResponse("Login error.", status=400)
+	else:
+		return HttpResponse("Error: missing password or username.", status=400)
